acc8cd334f9fe13578119f821fc7f3cf706ff1ab
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-11-14 22:57:25 +0400

    add __layout and about to routes

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<header>
+  <h1>Welcome</h1>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
+</header>
+
+<main>
+  <slot />
+</main>
+
+
+<footer>
+  Copyright 2021
+</footer>
---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quo commodi magnam sed rerum et dolorum odit neque ullam consequatur? Rem fuga provident sequi voluptates nemo eos esse consectetur dolorum?</p>

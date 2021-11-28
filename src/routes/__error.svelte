7cb67e89436b4ece846f68bf444b3ccba1cb3a9f
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-11-28 17:42:42 +0400

    add custom error page layout

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        error,
+        status
+      }
+    }
+  }
+</script>
+
+<script>
+  export let error;
+  export let status;
+</script>
+
+<h1>An Error happened</h1>
+
+<h3>{error.message}</h3>
+<h3>{status}</h3>

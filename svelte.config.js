4a8c2ae2ff0c0fa7e9992b076c5900328cc2cede
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-02 12:36:42 +0400

    install node adapter to build, in order to perform client side rendering

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import mdsvexConfig from './mdsvex.config.js';
 /** @type {import('@sveltejs/kit').Config} */
 
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
